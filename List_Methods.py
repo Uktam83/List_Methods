58f5c39696518529d49ec7fff39f17fc9ac3d8cd
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2024-02-23 09:13:14 +0500

    add

---
+++ List_Methods.py
@@ -0,0 +1,40 @@
+# #Methods name -> append()
+#my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+# my_list.append(7)
+# print(my_list)#[3, 1, 4, 1, 5, 9, 2, 6, 5,7]
+
+# # #Methods name ->clear()
+# my_list.clear()
+# print(my_list)#[]
+
+# # #Methods name ->extend()
+# my_list.extend([1, 2, 3])
+# print(my_list)#[3,1,4,1,5,9,2,6,5,1,2,3]
+
+## # #Methods name ->count()
+#print(my_list.count(2))#2
+#
+# # #Methods name -> index()
+#print(my_list.index(2))#6
+#
+ #Methods name -> insert()
+#my_list.insert(1, 4)
+#print(my_list)#[3, 4, 1, 4, 1, 5, 9, 2, 6, 5]
+#
+#Methods name -> pop()
+#my_list.pop(1)
+#print(my_list)#[3, 4, 1, 5, 9, 2, 6, 5]
+
+#Methods name ->remove()
+# my_list.remove(2)
+# print(my_list)#[3, 1, 4, 1, 5, 9, 6, 5]
+
+#
+#Methods name ->reverse()
+#my_list.reverse()
+#print(my_list)#[5, 6, 2, 9, 5, 1, 4, 1, 3]
+#
+#Methods name ->sort()
+# my_list.sort()
+# print(my_list)#[5, 6, 2, 9, 5, 1, 4, 1, 3]
+
